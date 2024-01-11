164102f25224c83c0d7bfbff450a386fe7ccd05b
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-01-11 14:01:20 +0100

    add vue3 app
    https://www.electronforge.io/guides/framework-integration/vue-3

--- src/renderer.js
+++ src/renderer.js
@@ -26,6 +26,12 @@
  * ```
  */
 
-import './index.css';
+// import './index.css';
 
-console.log('👋 This message is being logged by "renderer.js", included via Vite');
+// console.log('👋 This message is being logged by "renderer.js", included via Vite');
+
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
